dc7b8fddae3cabce100f184a988878780fbf2165
Vitalyy <vitalyy@bk.ru>
2022-11-14 22:53:45 +0300

    create models

--- stock_analysis/stock/admin.py
+++ stock_analysis/stock/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Ticker, History, News
 
-# Register your models here.
+
+class TickerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ticker']
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'sentiment', 'date', 'time']
+
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'positive_news', 'negative_news', 'date_history']
+
+
+admin.site.register(Ticker, TickerAdmin)
+admin.site.register(History, HistoryAdmin)
+admin.site.register(News, NewsAdmin)
--- stock_analysis/stock/models.py
+++ stock_analysis/stock/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticker(models.Model):
+    ticker = models.CharField(max_length=10, verbose_name='Тикер', default='')
+
+    class Meta:
+        verbose_name = 'Тикер'
+        verbose_name_plural = 'Тикеры'
+
+    def __str__(self):
+        return self.ticker
+
+
+class News(models.Model):
+    ticker = models.ForeignKey(Ticker, verbose_name='Тикер', on_delete=models.CASCADE)
+    title = models.TextField(verbose_name='Заголовок', default='')
+    sentiment = models.IntegerField(verbose_name='сентимен-анализ')
+    date = models.DateField(verbose_name='дата новости', auto_now_add=True, null=True)
+    time = models.TimeField(verbose_name='время добавления', auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name = 'Новости'
+        verbose_name_plural = 'Новости'
+
+    def __str__(self):
+        return f'{self.ticker} - {self.title[:50]} - {str(self.sentiment)}'
+
+
+class History(models.Model):
+    ticker = models.ForeignKey(Ticker, verbose_name='Тикер', on_delete=models.CASCADE)
+    positive_news = models.IntegerField(verbose_name='позитивные новости')
+    negative_news = models.IntegerField(verbose_name='негативные новости')
+    date_history = models.DateField(verbose_name='дата новости', null=True)
+
+    class Meta:
+        verbose_name = 'История'
+        verbose_name_plural = 'Истоия'
+
+    def __str__(self):
+        return f'{self.ticker} - pos: {str(self.positive_news)} - neg: {str(self.negative_news)} - {self.date_history}'
