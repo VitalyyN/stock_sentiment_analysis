d9f4c939e09d328cddbad9c0b57c61d414d19127
vitalyy <vitalyy@bk.ru>
2023-10-30 22:58:04 +0300

    optimized ORM, annotations

--- stock_analysis/stock/utils.py
+++ stock_analysis/stock/utils.py
@@ -1,15 +1,20 @@
+from typing import List, Dict
 import dotenv
 import re
 import requests
-import feedparser
 from datetime import datetime
+
+import feedparser
+import transformers
 from dateutil.parser import parse
 from bs4 import BeautifulSoup
+from django.db.models import Count, QuerySet, Model
 from newspaper import Article
 from newspaper.article import ArticleException
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+from .models import News
 
 dotenv.load_dotenv()
 
@@ -17,41 +22,88 @@ model = AutoModelForSequenceClassification.from_pretrained('stock/pt_save_pretra
 tokenizer = AutoTokenizer.from_pretrained('stock/tokenizer_save_pretrained')
 classifier = pipeline(task='sentiment-analysis', model=model, tokenizer=tokenizer)
 
-
 labels_dict = {0: 'neutral', 1: 'positive', 2: 'negative'}
 
 
-def get_request(tickers, news_obj, news_aggr, tickers_list):
+class NewsAggregator:
+    rss_link_rbc = 'https://rssexport.rbc.ru/rbcnews/news/20/full.rss'
+    rss_link_finam = 'https://www.finam.ru/analysis/conews/rsspoint/'
+    tg_chanel = 'https://tgstat.ru/channel/@cbrstocks'
+
+    source_finam = 'finam'
+    source_rbc = 'rbc'
+    source_tg = 'tg'
+
+    def __init__(self, rbc=True, finam=True, tg=True):
+        self.rbc_available = rbc
+        self.finam_available = finam
+        self.tg_available = tg
+
+    def aggregate(self, last_news: Dict, ticker_db: QuerySet):
+        all_news = list()
+        if self.rbc_available:
+            news_rbc = rss_parser(self.rss_link_rbc, self.source_rbc, last_news[self.source_rbc], ticker_db)
+            all_news.extend(news_rbc)
+        if self.finam_available:
+            news_finam = rss_parser(self.rss_link_finam, self.source_finam, last_news[self.source_finam],
+                                    ticker_db)
+            all_news.extend(news_finam)
+        if self.tg_available:
+            news_tg = tg_parser(self.tg_chanel, self.source_tg, last_news[self.source_tg], ticker_db)
+            all_news.extend(news_tg)
+
+        return all_news
+
+
+def get_request(tickers: QuerySet, news_obj: News, news_aggr: NewsAggregator, tickers_list: List[str]):
     last_news = dict()
-    last_news['rbc'] = news_obj.objects.filter(source=news_aggr.source_rbc).first()
-    last_news['finam'] = news_obj.objects.filter(source=news_aggr.source_finam).first()
-    last_news['tg'] = news_obj.objects.filter(source=news_aggr.source_tg).first()
+    last_news['rbc'] = news_obj.objects.filter(source=news_aggr.source_rbc).only('title').first()
+    last_news['finam'] = news_obj.objects.filter(source=news_aggr.source_finam).only('title').first()
+    last_news['tg'] = news_obj.objects.filter(source=news_aggr.source_tg).only('title').first()
 
     data = news_aggr.aggregate(last_news, tickers)
     save_in_db(news_obj, data)
 
     sentiment_list = count_of_sentiment(tickers_list, news_obj)
-    return sentiment_list, data
+    return sentiment_list
 
 
-def count_of_sentiment(ticker_list, db_model, return_dict=None):
-    res_list = list()
+def count_of_sentiment(ticker_list: List[str], db_model: News):
+    counts_sent = ((db_model.objects.values('ticker__ticker', 'sentiment_label').
+                    filter(ticker__ticker__in=ticker_list).
+                    annotate(count_sent=Count('sentiment_label', distinct=True))).
+                   filter(sentiment_label__in=[1, 2]))
+
+    ticker_dict = {}
+
     for ticker in ticker_list:
-        query_set_filtered = db_model.objects.filter(ticker__ticker__exact=ticker)
-        count_positive = query_set_filtered.filter(sentiment_label=1).count()
-        count_negative = query_set_filtered.filter(sentiment_label=2).count()
-        res_list.append({ticker: {'positive': count_positive, 'negative': count_negative}})
+        ticker_dict[ticker] = {'positive': 0, 'negative': 0}
 
+    for elem in counts_sent:
+        ticker = elem.get('ticker__ticker')
+        if elem.get('sentiment_label') == 1:
+            ticker_dict[ticker]['positive'] = elem.get('count_sent')
+        if elem.get('sentiment_label') == 2:
+            ticker_dict[ticker]['negative'] = elem.get('count_sent')
+
+    res_list = [{key: value} for key, value in ticker_dict.items()]
     return res_list
 
 
-def sentiment_analise(clf, message):
+def sentiment_analise(clf: transformers.pipelines, message: str):
     result = clf([message])
     sent_int = int(result[0]['label'][-1])
     return sent_int, labels_dict[sent_int], round(result[0]['score'], 2)
 
 
-def create_dict_for_db(title, source, datetime_news, ticker, sentiment_int, sentiment_str, score):
+def create_dict_for_db(title: str,
+                       source: str,
+                       datetime_news: datetime,
+                       ticker: str,
+                       sentiment_int: int,
+                       sentiment_str: str,
+                       score: float):
+
     news_item = {'ticker': ticker,
                  'title': title,
                  'source': source,
@@ -62,26 +114,30 @@ def create_dict_for_db(title, source, datetime_news, ticker, sentiment_int, sent
     return news_item
 
 
-def save_in_db(db_model, data_dict_list):
+def save_in_db(db_model: News, data_dict_list: List):
+    list_db_obj = []
     for elem in data_dict_list:
-        db_model.objects.create(ticker=elem['ticker'],
-                                title=elem['title'],
-                                source=elem['source'],
-                                sentiment_label=elem['sentiment_int'],
-                                sentiment_str=elem['sentiment_str'],
-                                sentiment_score=elem['score'],
-                                date_time=elem['date_time']
-                                )
+        db_elem = db_model(
+                ticker=elem['ticker'],
+                title=elem['title'],
+                source=elem['source'],
+                sentiment_label=elem['sentiment_int'],
+                sentiment_str=elem['sentiment_str'],
+                sentiment_score=elem['score'],
+                date_time=elem['date_time']
+            )
+        list_db_obj.append(db_elem)
+    db_model.objects.bulk_create(list_db_obj)
 
 
-def check_news_by_key(key_list, text):
+def check_news_by_key(key_list: List[str], text: str):
     for key in key_list:
         if text.find(key.strip()) != -1:
             return True
     return False
 
 
-def check_news(news_all, ticker_db, title, datetime_news, source):
+def check_news(news_all: List, ticker_db: QuerySet, title: str, datetime_news: datetime, source: str):
     for ticker_item in ticker_db:
         if datetime.now().date() == datetime_news.date() \
                 and check_news_by_key(ticker_item.key_word.split(','), title):
@@ -89,13 +145,13 @@ def check_news(news_all, ticker_db, title, datetime_news, source):
             news_all.append(create_dict_for_db(title, source, datetime_news, ticker_item, sent_int, sent_str, score))
 
 
-def rss_parser(url, source, last_news, ticker_db):
+def rss_parser(url: str, source: str, last_news: News, ticker_db: QuerySet):
     res = requests.get(url)
     feed = feedparser.parse(res.text)
     news_all = list()
 
     for entry in feed.entries:
-        title = None
+        title = ''
 
         if source == 'rbc':
             title = re.sub("[^А-Яа-яA-Za-z%0-9.,]", " ", entry['description'].lower())
@@ -112,7 +168,7 @@ def rss_parser(url, source, last_news, ticker_db):
     return news_all
 
 
-def tg_parser(url, source, last_news, ticker_db):
+def tg_parser(url: str, source: str, last_news: News, ticker_db: QuerySet):
     news_all = list()
     article = Article(url)
 
@@ -144,33 +200,3 @@ def tg_parser(url, source, last_news, ticker_db):
             continue
 
     return news_all
-
-
-class NewsAggregator:
-    rss_link_rbc = 'https://rssexport.rbc.ru/rbcnews/news/20/full.rss'
-    rss_link_finam = 'https://www.finam.ru/analysis/conews/rsspoint/'
-    tg_chanel = 'https://tgstat.ru/channel/@cbrstocks'
-
-    source_finam = 'finam'
-    source_rbc = 'rbc'
-    source_tg = 'tg'
-
-    def __init__(self, rbc=True, finam=True, tg=True):
-        self.rbc_available = rbc
-        self.finam_available = finam
-        self.tg_available = tg
-
-    def aggregate(self, last_news, ticker_db):
-        all_news = list()
-        if self.rbc_available:
-            news_rbc = rss_parser(self.rss_link_rbc, self.source_rbc, last_news[self.source_rbc], ticker_db)
-            all_news.extend(news_rbc)
-        if self.finam_available:
-            news_finam = rss_parser(self.rss_link_finam, self.source_finam, last_news[self.source_finam],
-                                    ticker_db)
-            all_news.extend(news_finam)
-        if self.tg_available:
-            news_tg = tg_parser(self.tg_chanel, self.source_tg, last_news[self.source_tg], ticker_db)
-            all_news.extend(news_tg)
-
-        return all_news
